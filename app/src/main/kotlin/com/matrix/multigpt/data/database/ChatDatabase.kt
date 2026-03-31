d749caac66fd99cb502be596389253f8a33e9cf6
itzharshraj123-ops <itzharshraj123@gmail.com>
2026-03-31 09:43:04 +0530

    Update ChatDatabase.kt

--- app/src/main/kotlin/com/matrix/multigpt/data/database/ChatDatabase.kt
+++ app/src/main/kotlin/com/matrix/multigpt/data/database/ChatDatabase.kt
@@ -5,14 +5,21 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.matrix.multigpt.data.database.dao.ChatRoomDao
 import com.matrix.multigpt.data.database.dao.MessageDao
+import com.matrix.multigpt.data.database.dao.NotificationLogDao
 import com.matrix.multigpt.data.database.entity.APITypeConverter
 import com.matrix.multigpt.data.database.entity.ChatRoom
 import com.matrix.multigpt.data.database.entity.Message
+import com.matrix.multigpt.data.database.entity.NotificationLog
 
-@Database(entities = [ChatRoom::class, Message::class], version = 2, exportSchema = false)
+@Database(
+    entities = [ChatRoom::class, Message::class, NotificationLog::class],
+    version = 3,
+    exportSchema = false
+)
 @TypeConverters(APITypeConverter::class)
 abstract class ChatDatabase : RoomDatabase() {
 
     abstract fun chatRoomDao(): ChatRoomDao
     abstract fun messageDao(): MessageDao
+    abstract fun notificationLogDao(): NotificationLogDao
 }
